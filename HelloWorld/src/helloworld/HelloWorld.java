a56c2c2cb82f42bdf965c671f80bba8d176c2dbf
2449875 <2449875@08-A103-MJ10711.SD41.ADS>
2021-02-18 10:42:13 -0800

    k

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -4,7 +4,38 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  
-	}
+	  System.out.println("I am AJ Dela Cruz");     
+ 
+ //      Answer for number 1:
+//	      A.0
+//	      b.2.0
+//	      c.0.25
+//        d. the answer is	error
+//	      e. 0.0
+	  
+          System.out.println(1/4);
+	      System.out.println(4.0/2);
+	      System.out.println(1.0/4.0);
+	      // int x = 4.0 / 2;
+	      double x = 1/4;
+	      System.out.println(x);
 
-}
+//        Answer for number 2:
+          int y = (int) Math.PI;
+	      System.out.println(y);
+//	          
+	      
+	      
+	      
+	      
+	      
+	      
+	      
+	      
+	   
+	}
+	
+	
+	}          	 
+	
+
